66b5c7d6de2dc6a192d9464d8d93585a6d3b1e19
Carsten Thiel <cat@schildwaechter.io>
2025-02-08 15:06:13 +0100

    Adding more routines

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 	"io"
 	"log"
 	"log/slog"
+	"math/rand/v2"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/ansrivas/fiberprometheus/v2"
 	"github.com/gofiber/contrib/otelfiber/v2"
@@ -20,6 +23,7 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
@@ -28,13 +32,17 @@ import (
 	otellog "go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
 
 	"go.opentelemetry.io/contrib/bridges/otelslog"
 )
 
-var buildEpoch string = "0"
+var (
+	buildEpoch string = "0"
+	tracer     trace.Tracer
+)
 
 // Get environment variable with a default
 func getEnv(name string, defaultValue string) string {
@@ -44,17 +52,17 @@ func getEnv(name string, defaultValue string) string {
 	}
 	return defaultValue
 }
-func InitTracer(otlphttpEndpoint string, serviceName string) (*trace.TracerProvider, error) {
+func InitTracer(otlphttpEndpoint string, serviceName string) (*sdktrace.TracerProvider, error) {
 	ctx := context.Background()
 	exporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpoint(otlphttpEndpoint), otlptracehttp.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
-	tracerProvider := trace.NewTracerProvider(
-		trace.WithSampler(trace.AlwaysSample()),
-		trace.WithBatcher(exporter),
-		trace.WithResource(resource.NewWithAttributes(
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
 			semconv.ServiceNameKey.String(serviceName))),
 	)
@@ -101,13 +109,67 @@ func InitLogger(otlphttpEndpoint string, serviceName string) (*otellog.LoggerPro
 	return logProvider, nil
 }
 
-func main() {
-	// our names
-	useName := getEnv("USENAME", "Genteel Beacon")
+func greaseGrate(ctx context.Context, tracer trace.Tracer) error {
+	_, span := tracer.Start(ctx, "Grease Grate")
+	defer span.End()
+
+	if rand.Float64() > 0.95 {
+		time.Sleep(8 * time.Millisecond) // artifical slowdown
+		err := errors.New("Grease Issue!")
+		//span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		slog.Error(err.Error())
+		return err
+	} else {
+		time.Sleep(1 * time.Millisecond) // artifical slowdown
+	}
+
+	return nil
+}
+
+func scribeStudy(ctx context.Context, tracer trace.Tracer, useName string, requestId string) string {
+	_, span := tracer.Start(ctx, "Scribe Study")
+	defer span.End()
+
 	nodeName, err := os.Hostname()
 	if err != nil {
 		nodeName = "unknown host"
 	}
+
+	time.Sleep(time.Duration(rand.IntN(100)+1) * time.Millisecond) // artifical slowdown
+	return "Build time: " + buildEpoch + ", »" + useName + "« running on " + nodeName + " 🙋 " + requestId
+}
+
+func courteousCourier(ctx context.Context, tracer trace.Tracer, client *http.Client, backend string) (bool, string) {
+	_, span := tracer.Start(ctx, "CourteousCourier")
+	defer span.End()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", backend+"/telegram", nil)
+
+	// Inject TraceParent to Context
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		span.RecordError(err)
+		slog.ErrorContext(ctx, "Error calling backend!")
+		return false, "Error calling backend!"
+	}
+	defer resp.Body.Close()
+
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		span.RecordError(err)
+		log.Fatal(err)
+		return false, ""
+	}
+
+	return true, string(responseData)
+}
+
+func main() {
+	// our names
+	useName := getEnv("USENAME", "Genteel Beacon")
 
 	app := fiber.New()
 	app.Use(requestid.New())
@@ -160,7 +222,7 @@ func main() {
 	prometheus.RegisterAt(app, "/metrics")
 
 	app.Use(prometheus.Middleware)
-	slog.SetDefault(otelslog.NewLogger(useName))
+	//slog.SetDefault(otelslog.NewLogger(useName))
 
 	_, jsonLogging := os.LookupEnv("JSONLOGGING")
 	var logger *slog.Logger
@@ -174,7 +236,8 @@ func main() {
 	} else {
 		logger = slog.New(
 			slogmulti.Fanout(
-				slog.Default().Handler(),
+				otelslog.NewLogger(useName).Handler(),
+				//slog.Default().Handler(),
 				slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}),
 			),
 		)
@@ -187,51 +250,38 @@ func main() {
 		Transport: otelhttp.NewTransport(http.DefaultTransport),
 	}
 
+	tracer = otel.Tracer("my tracer")
+
 	// Define a route for the root path '/'
 	app.Get("/telegram", func(c *fiber.Ctx) error {
-		tracer := otel.Tracer("telegram")
 		ctx, span := tracer.Start(c.UserContext(), "Telegram Endpoint")
 		span.SetAttributes(attribute.String("RequestID", slogfiber.GetRequestIDFromContext(c.Context())))
 		defer span.End()
+
+		greaseGrate(ctx, tracer)
 
 		var backendResponseString string
 		var backendResponse bool = false
 		backend, ok := os.LookupEnv("BACKEND")
 		if ok {
 
-			req, err := http.NewRequestWithContext(ctx, "GET", backend+"/telegram", nil)
-
-			// Inject TraceParent to Context
-			otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
-
-			resp, err := client.Do(req)
-			if err != nil {
-				span.RecordError(err)
-				return c.Status(http.StatusInternalServerError).SendString("Error calling backend!")
-			}
-			defer resp.Body.Close()
-
-			responseData, err := io.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			backendResponse = true
-			backendResponseString = string(responseData)
+			backendResponse, backendResponseString = courteousCourier(ctx, tracer, client, backend)
 
 		} else {
 			slog.Debug("No backend defined")
 		}
 
+		scribeStudyMessage := scribeStudy(ctx, tracer, useName, slogfiber.GetRequestIDFromContext(c.Context()))
 		if backendResponse {
-			return c.SendString("Build time: " + buildEpoch + ", »" + useName + "« running on " + nodeName + " 🙋 " + slogfiber.GetRequestIDFromContext(c.Context()) + " 📫 " + backendResponseString)
+			return c.SendString(scribeStudyMessage + " 📫 " + backendResponseString)
 
 		} else {
-			return c.SendString("Build time: " + buildEpoch + ", »" + useName + "« running on " + nodeName + " 🙋 " + slogfiber.GetRequestIDFromContext(c.Context()) + " 🏁")
+			return c.SendString(scribeStudyMessage + " 🏁")
 		}
 	})
 
 	// Start the server on the specified port and address
 	appPort := getEnv("APP_PORT", "1333")
-	appAddr := getEnv("APP_ADDR", "1333")
+	appAddr := getEnv("APP_ADDR", "0.0.0.0")
 	log.Fatal(app.Listen(appAddr + ":" + appPort))
 }
